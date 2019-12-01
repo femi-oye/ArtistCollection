008dc462bfdaf70cb3dfa17ddf3407556bca32f8
Femi Oye <femoye@Femis-MacBook-Pro-16.local>
2019-11-30 22:50:14 -0600

    Added bodyParser to server file and added POST methods to server file

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5002;
 
@@ -6,12 +7,20 @@ const artists = [{name: 'John Doe', song: 'Everything', year: '2001'},
                     {name: 'Jane Doe', song: 'All I Need Is You', year: '1998'},
                     {name: 'Jack Doe', song: 'In the Beginning', year: '1984'}];
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.use(express.static('server/public'));
 
 app.get('/artists', (req, res) => {
     res.send(artists);
     console.log(artists);
 });
+
+app.post('/artists', (req, res) => {
+    console.log(req.body);
+    artists.push(req.body);
+    res.sendStatus(201);
+})
 
 app.listen(PORT, function() {
     console.log(`listening on port ${PORT}`);
